1fe15aef823a3373579f17ea4c11fed5571909f4
mike_neck <jkrt3333@gmail.com>
2020-03-22 20:32:11 +0900

    Add class which represent engine execution.

--- ko-check-engine/src/main/kotlin/org/mikeneck/check/engine/exec/EngineExecution.kt
+++ ko-check-engine/src/main/kotlin/org/mikeneck/check/engine/exec/EngineExecution.kt
@@ -4,40 +4,39 @@ import org.junit.platform.engine.TestDescriptor
 import org.junit.platform.engine.TestSource
 import org.junit.platform.engine.TestTag
 import org.junit.platform.engine.UniqueId
-import org.junit.platform.engine.support.descriptor.ClassSource
 import org.mikeneck.check.Test
 import org.mikeneck.check.engine.Execution
 import org.mikeneck.check.engine.ExecutionListener
 import java.util.*
 
-class TestExecution(
-    private val parentExecution: Execution,
-    private val test: Test
+class EngineExecution(
+    private val rootUniqueId: UniqueId,
+    private val allTests: Iterable<Test>
 ): Execution {
 
   override fun execute(listener: ExecutionListener) = children().forEach { it.execute(listener) }
 
-  override fun children(): Iterable<Execution> = test.all.map { CheckExecution(this, test, it) }
+  override fun children(): Iterable<Execution> = allTests.map { TestExecution(this, it) }
 
-  override fun getSource(): Optional<TestSource> = Optional.of(ClassSource.from(test.javaClass.canonicalName))
+  override fun getSource(): Optional<TestSource> = Optional.empty()
 
   override fun removeFromHierarchy() = Unit
 
   override fun setParent(parent: TestDescriptor?) = Unit
 
-  override fun getParent(): Optional<TestDescriptor> = Optional.of(parentExecution)
+  override fun getParent(): Optional<TestDescriptor> = Optional.empty()
 
-  override fun getDisplayName(): String = test.javaClass.simpleName
+  override fun getDisplayName(): String = "ko-check"
 
   override fun getType(): TestDescriptor.Type = TestDescriptor.Type.CONTAINER
 
-  override fun getUniqueId(): UniqueId = parentExecution.uniqueId.append("test", displayName)
+  override fun getUniqueId(): UniqueId = rootUniqueId.append("ko-check", "engine-execution")
 
   override fun removeChild(descriptor: TestDescriptor?) = Unit
 
   override fun addChild(descriptor: TestDescriptor?) = Unit
 
-  override fun findByUniqueId(uniqueId: UniqueId?): Optional<out TestDescriptor> =
+  override fun findByUniqueId(uniqueId: UniqueId?): Optional<out TestDescriptor> = 
       when (uniqueId) {
         this.uniqueId -> Optional.of(this)
         else -> children().map { it.findByUniqueId(uniqueId) }.find { it.isPresent } ?: Optional.empty()
