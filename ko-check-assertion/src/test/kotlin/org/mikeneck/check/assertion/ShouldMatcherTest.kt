71c7298433ab9c8ed825b329c4d6f7a6ed9ab3bf
mike_neck <jkrt3333@gmail.com>
2020-03-26 01:05:11 +0900

    Add should containAll test

--- ko-check-assertion/src/test/kotlin/org/mikeneck/check/assertion/ShouldMatcherTest.kt
+++ ko-check-assertion/src/test/kotlin/org/mikeneck/check/assertion/ShouldMatcherTest.kt
@@ -10,3 +10,10 @@ by Given("1", { 1 })
     .Then("success", successCase(1, 1))
     .When("assert it with 'should be(2)'", { one -> one should be(2) })
     .Then("failure", failCase(1, 2))
+
+object ShouldContainAllTest: Test
+by Given("[foo,bar,qux]", { listOf("foo", "bar", "qux") })
+    .When("assert it with should containAll(foo,bar)'", { list -> list should containAll("foo", "bar") })
+    .Then("success", successCase(listOf("foo", "bar", "qux"), listOf("foo", "bar")))
+    .When("assert it with 'should containAll(bar,baz)'", { list -> list should containAll("bar", "baz") })
+    .Then("failure", failCase(listOf("foo", "bar", "qux"), listOf("bar", "baz")))
