5a4dead4f5379a288271d39aa7fafbaa9ba26d6f
mike_neck <jkrt3333@gmail.com>
2020-03-25 09:34:36 +0900

    Add Matcher type and one of its implementation

---
+++ ko-check-assertion/src/main/kotlin/org/mikeneck/check/assertion/Matcher.kt
@@ -0,0 +1,21 @@
+package org.mikeneck.check.assertion
+
+import org.mikeneck.check.Assertion
+
+interface Matcher<T> {
+  fun perform(actual: T): Assertion
+}
+
+fun <T: Any> containAll(vararg items: T): Matcher<Iterable<T>> = object : Matcher<Iterable<T>> {
+  override fun perform(actual: Iterable<T>): Assertion =
+      actual.toSet()
+          .let { act ->
+            when { 
+              act.containsAll(items.toList()) -> Assertion.success()
+              else ->
+                items.mapIndexed { index, item -> index to item }
+                    .filter { pair -> !act.contains(pair.second) }
+                    .joinToString(", ", "[", "]") { pair -> "${pair.second}(index:${pair.first})" }
+                    .let { Assertion.fail(actual, "contains ${items.toList()}\n,  but $it is not found") }
+            } }
+}
