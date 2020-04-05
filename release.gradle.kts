0082e56e4ef206c49b8e3ff2978a2b8eaed5f052
mike_neck <jkrt3333@gmail.com>
2020-04-05 23:31:40 +0900

    gradle script for release.

---
+++ release.gradle.kts
@@ -0,0 +1,76 @@
+plugins.apply("maven-publish")
+
+fun Project.publishing(configure: PublishingExtension.() -> Unit): Unit =
+    (this as ExtensionAware).extensions.configure("publishing", configure)
+val Project.publishing: PublishingExtension get() =
+  (this as ExtensionAware).extensions.getByName("publishing") as PublishingExtension
+
+publishing {
+  repositories { 
+    maven {
+      name = "sonatype"
+      val sonatypeUrl: String by project
+      url = java.net.URI.create(sonatypeUrl)
+      val sonatypeUsername: String? by project
+      val sonatypePassword: String? by project
+      credentials { 
+        username = sonatypeUsername
+        password = sonatypePassword
+      }
+    }
+  }
+  publications {
+    create<MavenPublication>("library") {
+      pom { 
+        description.set("Testing framework on JUnit Platform with Give-When-Then style.")
+        url.set("https://github.com/mike-neck/ktcheck")
+
+        licenses { 
+          license { 
+            name.set("MIT License")
+            url.set("https://opensource.org/licenses/mit-license.php")
+          }
+        }
+        developers { 
+          developer { 
+            id.set("mike-neck")
+            name.set("Shinya Mochida")
+            email.set("jkrt3333[at]gmail.com")
+          }
+        }
+        scm { 
+          connection.set("scm:git:https://github.com/mike-neck/ktcheck.git")
+          developerConnection.set("scm:git:https://github.com/mike-neck/ktcheck.git")
+          url.set("https://github.com/mike-neck/ktcheck")
+        }
+      }
+    }
+  }
+}
+
+val publicKey: String? by project
+val privateKey: String? by project
+if (publicKey != null && privateKey != null) {
+  plugins.apply("signing")
+
+  fun Project.signing(configure: SigningExtension.() -> Unit): Unit =
+      (this as ExtensionAware).extensions.configure("signing", configure)
+
+  signing {
+    useInMemoryPgpKeys(publicKey, privateKey)
+    sign(publishing.publications["sonatype"])
+  }
+}
+
+task("showType") {
+  doLast {
+    val publishToMavenLocal = tasks.getByPath("publish")
+    println(publishToMavenLocal.javaClass.canonicalName)
+    println(publishToMavenLocal.group)
+    println(publishToMavenLocal.dependsOn.map { it.toString() })
+  }
+}
+
+if (!project.hasProperty("KT_CHECK_VERSION")) {
+  tasks.find { it.name == "publishLibraryPublicationToSonatypeRepository" }?.enabled = false
+}
